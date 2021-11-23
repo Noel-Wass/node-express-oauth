c9f7238d0b44e648c15f6d82e8d2bab7454f758a
Noel Wass <focusonfundraising@xtra.co.nz>
2021-11-24 11:51:28 +1300

    app.post outline added; post to /approve endpoint; code on (req, res)=>{} not yet added.

--- authorization-server.js
+++ authorization-server.js
@@ -78,6 +78,10 @@ app.get("/authorize", (req, res) => {
     
 })
 
+app.post("/approve", (req, res) => {
+
+})
+
 const server = app.listen(config.port, "localhost", function () {
 	var host = server.address().address
 	var port = server.address().port
