b50499baafef31d2a4d27d3c272ddac62eaa0540
Noel Wass <focusonfundraising@xtra.co.nz>
2021-11-24 17:05:26 +1300

    const url = require("url") added to autthorization-server.js

--- authorization-server.js
+++ authorization-server.js
@@ -2,6 +2,7 @@ const fs = require("fs")
 const express = require("express")
 const bodyParser = require("body-parser")
 const jwt = require("jsonwebtoken")
+const url = require("url")
 const {
 	randomString,
 	containsAll,
