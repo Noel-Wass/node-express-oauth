51e0af77bd969cdfac13a82dbffa6855ba65d442
Noel Wass <focusonfundraising@xtra.co.nz>
2021-11-29 12:04:59 +1300

    Spelling error corrected.

--- client.js
+++ client.js
@@ -44,7 +44,7 @@ app.get('/authorize', (req, res) => {
 })
 
 
-app.get('/callback', (eq, res) => {
+app.get('/callback', (req, res) => {
 	if (!req.query.state || req.query.state !== state) {
 		res.status(403).send('Error: client has insufficent permissions to access resource');
 		return;
