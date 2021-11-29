195e1acceef6fb14103ba710d7777d94c68fae65
Noel Wass <focusonfundraising@xtra.co.nz>
2021-11-29 19:16:40 +1300

    Removed unnecessary import in authorization-server.js.

--- authorization-server.js
+++ authorization-server.js
@@ -3,7 +3,7 @@ const express = require("express")
 const bodyParser = require("body-parser")
 const jwt = require("jsonwebtoken")
 const url = require("url")
-const private_key = import ('./assets/private_key.pm')
+
 
 const {
 	randomString,
